5a7dcc26c5c8a91c5a4fe32ac9b6840a9da6ac19
MAVYan <yannis1.maving@epitech.eu>
2024-10-29 15:10:16 +0100

    color box and text

--- lib/TopNavigationBar.dart
+++ lib/TopNavigationBar.dart
@@ -18,32 +18,46 @@ class TopNavigationBar extends StatelessWidget implements PreferredSizeWidget {
       actions: [
         Padding(
           padding: const EdgeInsets.only(right: 16.0),
-          child: ElevatedButton(
-            onPressed: () {
-              // Action when the button is tapped
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFFFE3BA), // Updated to backgroundColor
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20), // Rounded corners
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFFFE3BA), // Light yellow on the left
+                  Color(0xFFFFD500), // More intense yellow on the right
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
               ),
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              borderRadius: BorderRadius.circular(20),
             ),
-            child: Row(
-              children: [
-                SvgPicture.asset(
-                  'lib/img/shop.svg', // Replace with the correct path
-                  height: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Action when the button is tapped
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent, // Transparent background
+                shadowColor: Colors.transparent, // No shadow
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                SizedBox(width: 8),
-                Text(
-                  'Boutique',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'lib/img/ticket_primary.svg', // Replace with the correct path
+                    height: 20,
                   ),
-                ),
-              ],
+                  SizedBox(width: 8),
+                  Text(
+                    'Boutique',
+                    style: TextStyle(
+                      color: Color(0xFF364F6B), // Updated text color to #364F6B
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
